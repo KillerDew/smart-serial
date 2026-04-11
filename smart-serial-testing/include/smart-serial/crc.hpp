0776eb1ceedad9a59bceb9d9e3debb2c0efb75ac
KillerDew <daniel.dew007@gmail.com>
2026-04-11 17:56:00 +0100

    Tested CRC-CCITT Test passed. ✅

---
+++ smart-serial-testing/include/smart-serial/crc.hpp
@@ -0,0 +1,49 @@
+/**
+ * @file crc.hpp
+ * @author Daniel Dew
+ * @brief Contains definition for CRC related utilities.
+ * @version 0.1
+ * @date 2026-04-11
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+
+#ifndef SMART_SERIAL_CRC_HPP
+#define SMART_SERIAL_CRC_HPP
+
+#include <cstdint>
+
+namespace Smart_serial { namespace CRC {
+
+    /**
+     * @brief Computes a CRC16 checksum for the given data buffer and length.
+     * 
+     * @param data data to compute CRC for
+     * @param length length of data buffer
+     * @return uint16_t the 16 bit CRC checksum
+     */
+    uint16_t compute_crc16(const uint8_t* const data, uint16_t length);
+
+    /**
+     * @brief Extracts the 16 bit CRC
+     * 
+     * @param data data to extract CRC from
+     * @param offset index at which the CRC is located in the data buffer
+     * @return uint16_t the extracted 16 bit CRC
+     */
+    uint16_t extract_crc16(uint8_t* data, uint16_t offset);
+
+    /**
+     * @brief appends a CRC to the buffer with low byte first
+     * 
+     * @param buf buffer to append CRC to
+     * @param offset index at which to insert the CRC
+     * @param crc 16 bit CRC to append
+     * @return uint16_t 1 if succesful, S_SERIAL_ERR if error.
+     */
+    uint16_t append_crc16(uint8_t* buf, uint16_t offset, uint16_t crc);
+
+}} // namespace CRC, Smart_serial
+
+#endif // SMART_SERIAL_CRC_HPP
--- smart-serial-testing/main.cpp
+++ smart-serial-testing/main.cpp
@@ -2,30 +2,13 @@
 #include <iostream>
 #include <conio.h>
 #include <iomanip>
+#include "smart-serial/crc.hpp"
 
 int main() {
-    Smart_serial::Mock_port<256U, 256U> mock_port;
-
-    uint8_t buf[] = {
-        0x00,
-        0x01,
-        0x02,
-        0x03,
-        0x04,
-        0x06,
-        0x09,
-        0x0A,
-        0x08,
+    const uint8_t buf[2] {
+        0xaa, 0xbb
     };
-    int32_t res = mock_port.write(buf, sizeof(buf));
-    std::cout << "Bytes written: " << res << std::endl;
-    mock_port.inject_rx(buf, sizeof(buf), false);
-
-    std::cout << "tx data: " << std::endl;
-    const uint8_t* myBytes = mock_port.get_tx_data();
-    for (size_t j = 0; j < 256U; ++j) {
-        std::cout << "0x" << std::hex << std::setw(2) << std::setfill('0') << (int)myBytes[j] << " ";
-    }
+    std::cout << Smart_serial::CRC::compute_crc16(buf, 2) << std::endl;
     getch();
     return 0;
 }
