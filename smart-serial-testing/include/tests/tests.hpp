2b409c0a7c22df4ba82fbe8023f73058c0b0389f
KillerDew <daniel.dew007@gmail.com>
2026-04-20 09:32:26 +0100

    moved CHECK() for tests to header

--- smart-serial-testing/include/tests/tests.hpp
+++ smart-serial-testing/include/tests/tests.hpp
@@ -1,4 +1,8 @@
 
+
+#define CHECK(label, cond, pass, fail) \
+        if (cond) { printf("PASS - %s\n", label); (*pass)++; } \
+        else      { printf("FAIL - %s\n", label); (*fail)++; }
 namespace Tests {
     int test_master();
     int test_slave();
