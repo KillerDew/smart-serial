a9de1b6497a1287e7d3a910af44eb7e4c0dd066e
Daniel Dew <daniel.dew007@gmail.com>
2026-05-03 14:33:21 +0100

    Added api.hpp

---
+++ smart-serial/include/smart-serial/api.hpp
@@ -0,0 +1,23 @@
+/**
+ * @file api.hpp
+ * @author Daniel Dew
+ * @brief Contains all headers needed to use smart serial from a PROS project
+ * @version 0.1
+ * @date 2026-05-03
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+
+#ifndef SMART_SERIAL_API_HPP
+#define SMART_SERIAL_API_HPP
+
+#include "./clock/Pros_clock.hpp"
+#include "./port/pros_serial_port.hpp"
+#include "./error.h"
+#include "./frame.hpp"
+#include "./master.hpp"
+#include "./slave.hpp"
+#include "./types.h"
+
+#endif // SMART_SERIAL_API_HPP
